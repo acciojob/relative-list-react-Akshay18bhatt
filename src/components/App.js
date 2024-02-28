680770c20d9c9165bc51397dea9bbbddac6e1410
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2024-02-28 07:20:15 +0000

    t1

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,14 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+               <ol key="relativeList" >
+                <li key="relativeListItem1" >tangyMango</li>
+                <li key="relativeListItem2" >purpleBaingan</li>
+                <li key="relativeListItem3" >teekhiChilli</li>
+                <li key="relativeListItem4" >kela</li>
+                <li key="relativeListItem5" >khattiImli</li>
+                <li key="relativeListItem6" >kalAana</li>
+               </ol>
     </div>
   )
 }
